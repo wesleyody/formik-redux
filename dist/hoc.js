859a445aa5b649936df75e063795d1c4b0329fd8
Wesley Ody <wesleyody@gmail.com>
2019-11-04 14:07:40 -0300

    dist: update

--- dist/hoc.js
+++ dist/hoc.js
@@ -20,6 +20,7 @@ function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { va
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
 var mapStateToProps = function mapStateToProps(state, props) {
+  console.log(state);
   return {
     submitting: (0, _selectors.isSubmitting)(props.form)(state),
     error: (0, _selectors.getSubmitError)(props.form)(state)
