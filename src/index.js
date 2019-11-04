5e2009b095bbcdc45cf96abda0515a9aab7d273a
Wesley Ody <wesleyody@gmail.com>
2019-11-04 17:21:18 -0300

    index: export reset action

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-export { startSubmit, stopSubmit } from "./actions";
+export { reset, startSubmit, stopSubmit } from "./actions";
 export { default as withForm } from "./hoc";
 export { default as reducer } from "./reducers";
 export { isSubmitting, getSubmitError } from "./selectors";
