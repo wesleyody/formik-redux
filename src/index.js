957c4cd13f53d7b066b6e862f8e106cfbd92e1af
Wesley Ody <wesleyody@gmail.com>
2019-11-04 14:56:22 -0300

    index: export selectors

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { startSubmit, stopSubmit } from "./actions";
 export { default as withForm } from "./hoc";
 export { default as reducer } from "./reducers";
+export { isSubmitting, getSubmitError } from "./selectors";
