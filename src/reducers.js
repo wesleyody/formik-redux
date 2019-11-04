293d9488a863c9689b96b1077e54bcf6f895cf74
Wesley Ody <wesleyody@gmail.com>
2019-11-04 10:15:10 -0300

    reducers: treats start and stop submit actions

---
+++ src/reducers.js
@@ -0,0 +1,24 @@
+import * as actions from "./actions";
+
+export default function ( state = {}, action ) {
+    switch ( action.type ) {
+        case actions.START_SUBMIT:
+            return {
+                ...state,
+                [ action.payload ]: {
+                    submitting: true,
+                    error: null
+                }
+            };
+        case actions.STOP_SUBMIT:
+            return {
+                ...state,
+                [ action.payload ]: {
+                    submitting: false,
+                    error: action.error
+                }
+            };
+        default:
+            return state;
+    }
+}
