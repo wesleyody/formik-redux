0e998667fd9e69472753e4727a866708250b3019
Wesley Ody <wesleyody@gmail.com>
2019-11-04 14:56:34 -0300

    hoc: check form prop

--- src/hoc.js
+++ src/hoc.js
@@ -1,21 +1,24 @@
+import PropTypes from "prop-types";
 import { withFormik } from "formik";
 import { connect } from "react-redux";
 import { compose } from "recompose";
 
 import { isSubmitting, getSubmitError } from "./selectors";
 
-const mapStateToProps = form => state => {
-    return ({
-        submitting: isSubmitting( form )( state ),
-        error: getSubmitError( form )( state )
-    });
-};
+const mapStateToProps = form => state => ({
+    submitting: isSubmitting( form )( state ),
+    error: getSubmitError( form )( state )
+});
 
 const Hoc = options => WrappedComponent => {
     return compose(
         connect( mapStateToProps( options.form ) ),
         withFormik({ displayName: options.form, ...options })
     )( WrappedComponent );
+};
+
+Hoc.propTypes = {
+    form: PropTypes.string.isRequired
 };
 
 export default Hoc;
